2783700c4883d749bbfd71193bf6e036f003e01f
LPenny <47389972+LPenny@users.noreply.github.com>
2020-04-25 11:37:44 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,35 @@
 <?php
 define('VERBOSE', 0);        // 0: be silent, except for errors; 1: be verbose
-define('IDCOUNT', N);
-define('APIKEY', 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
-define('SYSTEMID', 'NNNNNN');
+define('IDCOUNT', 16);
+define('APIKEY', '573d441162d2aebfdb93a5979e360f8d49f63355');
+define('SYSTEMID', 'LP1');
 
 define('LIFETIME', 1);       // see README.md
-define('MODE', 'AGGREGATE'); // 'AGGREGATE' or 'SPLIT'
+define('MODE', 'SPLIT'); // 'AGGREGATE' or 'SPLIT'
 define('EXTENDED', 0);       // Send state data? Uses donation only feature
 // AC is default 0. See README.md
 define('AC', 0);             // Send DC data or AC (DC * Efficiency)
 
 // If mode is SPLIT, define the Enecsys ID to PVOutput SystemID mapping for each
 // inverter.
-//$systemid = array(
-//  NNNNNNNNN => NNNNNN,
-//  NNNNNNNNN => NNNNNN,
-//  ...
-//);
+$systemid = array(
+  110087366 => LP1,
+  110087174 => LP1,
+  110087243 => LP1,
+  110087250 => LP1,
+  110060744 => LP1,
+  110061494 => LP1,
+  110087360 => LP1,
+  110087240 => LP1,
+  110074612 => LP1,
+  110087176 => LP1,
+  110087191 => LP1,
+  110087184 => LP1,
+  110087219 => LP1,
+  110087246 => LP1,
+  110087169 => LP1,
+  110087237 => LP1
+);
 
 // If mode is SPLIT, optionally define the Enecsys ID to APIKEY mappings
 // If an id is not found, the default APIKEY from above is used.
@@ -26,10 +39,10 @@ define('AC', 0);             // Send DC data or AC (DC * Efficiency)
 //);
 
 // The following inverter ids are ignored (e.g. the neighbours' ones)
-$ignored = array(
+// $ignored = array(
 // NNNNNNNNN,
 // ...
-);
+// );
 
 
 // Optional MySQL defs, uncomment to enable MySQL inserts, see README.md
